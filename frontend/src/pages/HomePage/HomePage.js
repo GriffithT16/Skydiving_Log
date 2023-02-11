324b35946ee3fcdfed1e1c4d41e161acbbedb366
Tyler Griffith <griffitht16@gmail.com>
2023-02-11 12:11:16 -0600

    Added a video to the HomePage

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -4,13 +4,13 @@ import useAuth from "../../hooks/useAuth";
 import { Link } from "react-router-dom";
 import axios from "axios";
 
-
 const HomePage = () => {
   // The "user" value from this Hook contains the decoded logged in user information (username, first name, id)
   // The "token" value is the JWT token that you will send in the header of any request requiring authentication
 
   const [user, token] = useAuth();
   const [jumps, setJumps] = useState([]);
+  const [youtubeID] = useState('p7bXOQb6R6o')
 
   useEffect(() => {
     fetchJumps();
@@ -32,11 +32,25 @@ const HomePage = () => {
   return (
     <div className="background-img2">
       <div className="container">
-        <h1 style={{color:"navy"}}>{user.username}'s Skydive Tracker</h1>
-        {jumps && jumps.map((jump) => <p key={jump.id} style={{color:"navy"}}>{jump.year}</p>)}
+        <h1 style={{ color: "navy" }}>{user.username}'s Skydive Tracker</h1>
+        {jumps &&
+          jumps.map((jump) => (
+            <p key={jump.id} style={{ color: "navy" }}>
+              {jump.year}
+            </p>
+          ))}
       </div>
+      <iframe
+        className="video"
+        style={{marginLeft: "40%", marginTop: "27rem", height:"12rem"}}
+        title="Youtube player"
+        sandbox="allow-same-origin allow-forms allow-popups allow-scripts allow-presentation"
+        src={`https://youtube.com/embed/${youtubeID}?autoplay=0`}
+      ></iframe>
     </div>
   );
 };
 
 export default HomePage;
+
+// ! "Swoop Video", https://youtube.com/shorts/p7bXOQb6R6o?feature=share
